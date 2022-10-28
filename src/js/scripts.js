65fc836e5c7254d3f931c629c92fd2d9f7d63d76
manisha sinha <bits.manisha@gmail.com>
2022-10-28 18:53:35 +0200

    Imported orbitcontrols and created orbit instance class

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import { OrbitControls, orbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
 const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -9,6 +11,8 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const orbit = new OrbitControls(camera, renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(3);
 scene.add(axesHelper);
