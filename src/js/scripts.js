1b84c8ece8e6ac4f626ef3e387fdaa30ca804bff
manisha sinha <bits.manisha@gmail.com>
2022-10-29 23:29:07 +0200

    Changed the mesh basic to standarMaterial.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,7 +39,7 @@ const gridHelper = new THREE.GridHelper(30);
 scene.add(gridHelper);
 
 const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0x0000ff, wireframe: true });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
